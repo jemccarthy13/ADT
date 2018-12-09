326adb191ff491b0a0964432ba490e2c3489b958
John McCarthy <jemccarthy13@gmail.com>
2018-12-08 20:40:59 -0600

    fixed logging

--- adt/utilities/Output.java
+++ adt/utilities/Output.java
@@ -13,24 +13,22 @@ public class Output {
 	/**
 	 * Show an information dialog
 	 * 
-	 * @param title
-	 *            - the title of the window
-	 * @param msg
-	 *            - the message to show
+	 * @param title - the title of the window
+	 * @param msg   - the message to show
 	 */
 	public static void showInfoMessage(String title, String msg) {
 		if (Configuration.getInstance().isShowMessages()) {
 			JOptionPane.showMessageDialog(null, msg, title, JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			DebugUtility.debug(Output.class, msg);
 		}
 	}
 
 	/**
 	 * Fore an information dialog to display
 	 * 
-	 * @param title
-	 *            - the title of the window
-	 * @param msg
-	 *            - the message to display
+	 * @param title - the title of the window
+	 * @param msg   - the message to display
 	 */
 	public static void forceInfoMessage(String title, String msg) {
 		JOptionPane.showMessageDialog(null, msg, title, JOptionPane.INFORMATION_MESSAGE);
@@ -39,10 +37,8 @@ public class Output {
 	/**
 	 * Show a confirmation dialog.
 	 * 
-	 * @param title
-	 *            - the title of the window
-	 * @param msg
-	 *            - the message to display
+	 * @param title - the title of the window
+	 * @param msg   - the message to display
 	 * @return - true iff "OK" was selected
 	 */
 	public static boolean showConfirmMessage(String title, String msg) {
