67d05692d24d7d9f7d4411b9f6fc4080ff1a5f67
John McCarthy <jemccarthy13@gmail.com>
2019-02-09 13:50:31 -0600

    ASmanager GUI/Model separation

---
+++ custom/swing/Borders.java
@@ -0,0 +1,15 @@
+package swing;
+
+import java.awt.Color;
+
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
+/**
+ * Holder for the Table borders
+ */
+public class Borders {
+
+	/** A plain black table border */
+	public static final Border BLACK = new LineBorder(Color.BLACK);
+}
